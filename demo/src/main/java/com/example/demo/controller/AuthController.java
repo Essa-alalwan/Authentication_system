7c24e02563fe589e78c91d6a175a31e974dc01c5
Essa_alalwan <essa.alalwan26@gmail.com>
2026-01-07 22:38:51 +0300

    Add authentication controller with register and login endpoints

--- demo/src/main/java/com/example/demo/controller/AuthController.java
+++ demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -5,7 +5,10 @@ import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
